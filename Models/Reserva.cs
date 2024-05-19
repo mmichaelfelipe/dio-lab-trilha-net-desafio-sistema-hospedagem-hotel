f067ad288096e4bbaff8d29d4fb10ca299e9104b
Michael Felipe <michaelfelipe.ti@gmail.com>
2024-05-19 14:04:34 -0300

    ♻️ refactor: migrates Model classes to global namespace
    Signed-off-by: Michael Felipe <michaelfelipe.ti@gmail.com>

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,50 +1,49 @@
-namespace DesafioProjetoHospedagem.Models
+namespace DesafioProjetoHospedagem.Models;
+
+public class Reserva
 {
-    public class Reserva
+    public List<Pessoa> Hospedes { get; set; }
+    public Suite Suite { get; set; }
+    public int DiasReservados { get; set; }
+
+    public Reserva() { }
+
+    public Reserva(int diasReservados)
     {
-        public List<Pessoa> Hospedes { get; set; }
-        public Suite Suite { get; set; }
-        public int DiasReservados { get; set; }
+        DiasReservados = diasReservados;
+    }
 
-        public Reserva() { }
-
-        public Reserva(int diasReservados)
+    public void CadastrarHospedes(List<Pessoa> hospedes)
+    {
+        if (hospedes.Count > Suite.Capacidade)
         {
-            DiasReservados = diasReservados;
+            throw new InvalidOperationException("A quantidade de hospedes excede a capacidade da suíte.");
+        }
+        else
+        {
+            Hospedes = hospedes;
+        }
+    }
+
+    public void CadastrarSuite(Suite suite)
+    {
+        Suite = suite;
+    }
+
+    public int ObterQuantidadeHospedes()
+    {
+        return Hospedes.Count;
+    }
+
+    public decimal CalcularValorDiaria()
+    {
+        decimal valor = DiasReservados * Suite.ValorDiaria;
+
+        if (DiasReservados >= 10)
+        {
+            valor = valor * 0.9M;
         }
 
-        public void CadastrarHospedes(List<Pessoa> hospedes)
-        {
-            if (hospedes.Count > Suite.Capacidade)
-            {
-                throw new InvalidOperationException("A quantidade de hospedes excede a capacidade da suíte.");
-            }
-            else
-            {
-                Hospedes = hospedes;
-            }
-        }
-
-        public void CadastrarSuite(Suite suite)
-        {
-            Suite = suite;
-        }
-
-        public int ObterQuantidadeHospedes()
-        {
-            return Hospedes.Count;
-        }
-
-        public decimal CalcularValorDiaria()
-        {
-            decimal valor = DiasReservados * Suite.ValorDiaria;
-
-            if (DiasReservados >= 10)
-            {
-                valor = valor * 0.9M;
-            }
-
-            return valor;
-        }
+        return valor;
     }
 }
